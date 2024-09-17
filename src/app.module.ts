3e0f6a53c0a5bb29e59884a4417ca0a95802e36e
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2024-09-18 00:48:18 +0600

    added middleware functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { BusModule } from './bus/bus.module';
 import { TrainModule } from './train/train.module';
 import { SeatModule } from './seat/seat.module';
-import { TicketService } from './ticket/ticket.service';
-import { TicketController } from './ticket/ticket.controller';
 import { LocationModule } from './location/location.module';
 import DatabaseConnection from './database/connection';
 import { TicketModule } from './ticket/ticket.module';
-import { UserController } from './user/user.controller';
-import { BusController } from './bus/bus.controller';
-import { TrainController } from './train/train.controller';
-import { SeatController } from './seat/seat.controller';
-import { LocationController } from './location/location.controller';
-import { UserService } from './user/user.service';
-import { BusService } from './bus/bus.service';
-import { TrainService } from './train/train.service';
-import { SeatService } from './seat/seat.service';
-import { LocationService } from './location/location.service';
-
+import { LoggerMiddleware } from './logger/logger.middleware';
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
@@ -36,6 +22,9 @@ import { LocationService } from './location/location.service';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
-//AppController, UserController,BusController,TrainController,SeatController,LocationController, TicketController
-//AppService,UserService,BusService,TrainService,SeatService,LocationService, TicketService
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/');
+    //another way to apply middleware : .forRoutes({ path: '[name of the path e.g. users]', method: RequestMethod.GET/POST/PUT/DELETE etc. can use ALL });
+  }
+}
