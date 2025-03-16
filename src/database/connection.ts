217ee62ce34ab1fccd747933634c70f451037c98
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2025-03-16 14:56:24 +0600

    changed conf

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,28 +1,26 @@
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { Bus } from "src/models/bus.model";
-import { Location } from "src/models/location.model";
-import { Payment } from "src/models/payment.model";
-import { Seat } from "src/models/seat.model";
-import { Ticket } from "src/models/ticket.model";
-import { Train } from "src/models/train.model";
-import { User } from "src/models/user.model";
-import * as fs from 'fs';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Bus } from 'src/models/bus.model';
+import { Location } from 'src/models/location.model';
+import { Payment } from 'src/models/payment.model';
+import { Seat } from 'src/models/seat.model';
+import { Ticket } from 'src/models/ticket.model';
+import { Train } from 'src/models/train.model';
+import { User } from 'src/models/user.model';
 
 export default (configService: ConfigService): TypeOrmModuleOptions => {
   const options: TypeOrmModuleOptions = {
     type: 'postgres',
     host: configService.get('DB_HOST'),
-    port: +configService.get<number>('DB_PORT'),//+ is to convert it to a number otherwise string as default
+    port: configService.get<number>('DB_PORT'), //+ is to convert it to a number otherwise string as default
     username: configService.get('DB_USER'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
     ssl: {
-      ca: configService.get('CA_CERT_PATH'),
-  },
-    entities: [User,Bus,Train,Ticket,Seat,Location,Payment],
+      ca: configService.get('CA_CERT'),
+    },
+    entities: [User, Bus, Train, Ticket, Seat, Location, Payment],
     synchronize: true,
-
-  }
+  };
   return options;
-}
+};
