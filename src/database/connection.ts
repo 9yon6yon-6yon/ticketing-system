a927ee1147a5a8c5ff60e664d292414dd4802a1a
Md. Asif Karim Chayon <79499779+9yon6yon-6yon@users.noreply.github.com>
2026-04-17 00:49:46 +0600

    Refactor DB_PORT retrieval for clarity

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -12,7 +12,7 @@ export default (configService: ConfigService): TypeOrmModuleOptions => {
   const options: TypeOrmModuleOptions = {
     type: 'postgres',
     host: configService.get('DB_HOST'),
-    port: configService.get<number>('DB_PORT'), //+ is to convert it to a number otherwise string as default
+    port: configService.get<number>('DB_PORT'), //+ is to convert it to a number
     username: configService.get('DB_USER'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
