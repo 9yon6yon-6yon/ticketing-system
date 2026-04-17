3abf377796cea2f039a4a03e51123ea4cda0f98d
9yon6yon-6yon <official.net.work.cn@gmail.com>
2026-04-17 19:41:27 +0600

    local deployment working

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -8,45 +8,17 @@ import { Ticket } from 'src/models/ticket.model';
 import { Train } from 'src/models/train.model';
 import { User } from 'src/models/user.model';
 
-// export default (configService: ConfigService): TypeOrmModuleOptions => {
-//   const db_URI = configService.get<string>('DB_URI');
-//   const options: TypeOrmModuleOptions = {
-//     type: 'postgres',
-//     host: configService.get('DB_HOST'),
-//     port: configService.get<number>('DB_PORT'), //+ is to convert it to a number
-//     username: configService.get('DB_USER'),
-//     password: configService.get('DB_PASSWORD'),
-//     database: configService.get('DB_DATABASE'),
-//     ssl: { rejectUnauthorized: false },
-//     entities: [User, Bus, Train, Ticket, Seat, Location, Payment],
-//     synchronize: true,
-//   };
-//   return options;
-// };
 export default (configService: ConfigService): TypeOrmModuleOptions => {
-  const dbURI = configService.get<string>('DB_URI');
-  console.log('DB_URI:', configService.get('DB_URI'));
-  if (dbURI) {
-    return {
-      type: 'postgres',
-      url: dbURI,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      entities: [User, Bus, Train, Ticket, Seat, Location, Payment],
-      synchronize: false, 
-    };
-  }
-
-  return {
+  const options: TypeOrmModuleOptions = {
     type: 'postgres',
     host: configService.get('DB_HOST'),
-    port: Number(configService.get<number>('DB_PORT')),
+    port: configService.get<number>('DB_PORT'), //+ is to convert it to a number
     username: configService.get('DB_USER'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
-    ssl: false,
+    ssl: { rejectUnauthorized: false },
     entities: [User, Bus, Train, Ticket, Seat, Location, Payment],
     synchronize: true,
   };
+  return options;
 };
--- src/main.ts
+++ src/main.ts
@@ -5,41 +5,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 class APIDocument {
   static setup(app: INestApplication) {
-    const options = new DocumentBuilder()
-      .setTitle('Ticket System API')
-      .setDescription('This application exposes the backend APIs for the Ticketing System')
-      .build();
-    const document = SwaggerModule.createDocument(app, options, {
-      ignoreGlobalPrefix: true,
-    });
-
+    const options = new DocumentBuilder().setTitle('Ticket System API').setDescription('This application exposes the backend APIs for the Ticketing System').build();
+    const document = SwaggerModule.createDocument(app, options, { ignoreGlobalPrefix: true })
     SwaggerModule.setup('API', app, document);
   }
 }
-
-let app: INestApplication;
-
-async function createApp() {
-  if (!app) {
-    app = await NestFactory.create(AppModule, { cors: true });
-
-    APIDocument.setup(app);
-
-    await app.init();
-  }
-  return app;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  APIDocument.setup(app);
+  await app.listen(3000);
 }
-
-export default async function handler(req, res) {
-  try {
-    const app = await createApp();
-    const server = app.getHttpAdapter().getInstance();
-    return server(req, res);
-  } catch (err) {
-    console.error('🔥 RUNTIME ERROR:', err);
-    res.status(500).json({
-      error: err.message,
-      stack: err.stack,
-    });
-  }
-}
+bootstrap();
