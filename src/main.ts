a2f8b5fd7a387a6936c55a6bc553dc0787ea18f3
9yon6yon-6yon <official.net.work.cn@gmail.com>
2026-04-20 13:31:34 +0600

    api naming changed

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ class APIDocument {
   static setup(app: INestApplication) {
     const options = new DocumentBuilder().setTitle('Ticket System API').setDescription('This application exposes the backend APIs for the Ticketing System').build();
     const document = SwaggerModule.createDocument(app, options, { ignoreGlobalPrefix: true })
-    SwaggerModule.setup('API', app, document);
+    SwaggerModule.setup('api', app, document);
   }
 }
 async function bootstrap() {
